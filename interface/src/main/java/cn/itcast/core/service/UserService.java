1fb101677712363b5a810d3062e435fab8c6ed9e
root <478670565@qq.com>
2019-01-11 15:53:59 +0800

    运营商后台-用户管理（冻结） 运营商后台-用户统计

--- interface/src/main/java/cn/itcast/core/service/UserService.java
+++ interface/src/main/java/cn/itcast/core/service/UserService.java
@@ -1,7 +1,9 @@
 package cn.itcast.core.service;
 
-import cn.itcast.core.pojo.entity.Result;
+import cn.itcast.core.pojo.entity.PageResult;
 import cn.itcast.core.pojo.user.User;
+
+import java.util.List;
 
 public interface UserService {
 
@@ -10,4 +12,15 @@ public interface UserService {
     public Boolean checkSmsCode(String phone, String smsCode);
 
     public void add(User user);
+
+    public List<User> findAll();
+
+    public PageResult findByPage(User user, Integer page, Integer rows);
+
+    public User findOne(Long id);
+
+    public void update(Long id, String status);
+
+    public PageResult search(User user, Integer page, Integer rows);
+
 }
